ee5c7159685110344e3bcae6a1fc91e239226180
jvrbnt <jvr.bnt.rnz@gmail.com>
2026-03-16 08:47:44 +0100

    models.py y schemas.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/")
-def read_root():
-    return {"MiNa_SGO": "Entorno Verificado"}
+async def read_index():
+    return FileResponse("static/ServicioLogin.html", )
