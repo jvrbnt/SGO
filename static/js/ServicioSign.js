8169ed6b8f66cd1a9e40ab3f1766ac04c3b6c790
miikee183 <miguelprorevolucio@gmail.com>
2026-03-16 10:18:21 +0100

    archivos ordenados por carpetas

--- static/js/ServicioSign.js
+++ static/js/ServicioSign.js
@@ -54,11 +54,11 @@ registroForm.addEventListener('submit', async function(event) {
 
         if (response.ok) {
             alert(data.mensaje); // "Cuenta creada con éxito"
-<<<<<<< HEAD:static/js/ServicioSign.js
+<<<<<<< HEAD://SGO/static/js/ServicioSign.js
             window.location.href = "/SGO/static/html/ServicioLogin.html";
 =======
-            window.location.href = "ServicioLogin.html";
->>>>>>> 1632cb6210aba5593f716fa3e7b26d0585289efe:static/ServicioSign.js
+            window.location.href = "/SGO/static/html/ServicioLogin.html";
+>>>>>>> 1632cb6210aba5593f716fa3e7b26d0585289efe:/SGO/static/js/ServicioSign.js
         } else {
             // El backend devuelve un error (ej: El email ya está registrado)
             alert(data.detail); 
